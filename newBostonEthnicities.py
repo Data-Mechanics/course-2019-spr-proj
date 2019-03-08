75fe7deb460f9d79205591848bc9921cff441dc9
dixyTW <tom851223@gmail.com>
2019-03-08 10:21:41 -0500

    Update newBostonEthnicities.py
    updated prov

--- newBostonEthnicities.py
+++ newBostonEthnicities.py
@@ -71,39 +71,30 @@ class newBostonEthnicities(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
+        repo.authenticate('dixyTW_veeyn', 'dixyTW_veeyn')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:alice_bob#getData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_found, this_script)
-        doc.wasAssociatedWith(get_lost, this_script)
-        doc.usage(get_found, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+        this_script = doc.agent('alg:dixyTW_veeyn#newBostonEthnicities', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('dat:dixyTW_veeyn#bostonEthnicities', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        transform_ethnicities = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(transform_ethnicities, this_script)
+        
+        doc.usage(transform_ethnicities, resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Calculation',
+                  'ont:Query':''
                   }
                   )
-        doc.usage(get_lost, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
-                  }
-                  )
+        
 
-        lost = doc.entity('dat:fat60221_veeyn#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_lost, endTime)
-        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+        ethnicityPop = doc.entity('dat:dixyTW_veeyn#ethnicityPop', {prov.model.PROV_LABEL:'Filter out outdated data and get the total population of an area', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(ethnicityPop, this_script)
+        doc.wasGeneratedBy(ethnicityPop, transform_ethnicities, endTime)
+        doc.wasDerivedFrom(ethnicityPop, resource, transform_ethnicities, transform_ethnicities, transform_ethnicities)
 
-        found = doc.entity('dat:fat60221_veeyn#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(found, this_script)
-        doc.wasGeneratedBy(found, get_found, endTime)
-        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
 
         repo.logout()
                   
