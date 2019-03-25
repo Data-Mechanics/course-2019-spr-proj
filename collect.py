027fa7dad66a2c314708a39d3f314d17f23cb12f
Tyler Lux <tlux@bu.edu>
2019-03-24 23:20:53 -0400

    files

---
+++ collect.py
@@ -0,0 +1,21 @@
+import urllib.request
+import json
+import dml
+
+url = "http://bostonopendata-boston.opendata.arcgis.com/datasets/b7739e6673104c048f5e2f28bb9b2281_0.geojson"
+
+response = urllib.request.urlopen(url).read()
+
+l = json.loads(response.decode('utf-8'))
+
+print(l.keys())
+
+url1 = "https://data.boston.gov/datastore/odata3.0/fd351943-c2c6-4630-992d-3f895360febd?format=json"
+response = urllib.request.urlopen(url1).read()
+client = dml.pymongo.MongoClient()
+repo = client.repo
+repo.authenticate('tlux', 'tlux')
+repo.dropCollection("test")
+repo.createCollection("test")
+repo['tlux.test'].insert_many(l['features'])
+repo['tlux.test'].metadata({'complete': True})
---
+++ test.py
@@ -0,0 +1,52 @@
+import dml
+import prov.model
+import datetime
+import uuid
+import json
+from shapely.geometry import Point, Polygon
+
+
+
+
+client = dml.pymongo.MongoClient()
+repo = client.repo
+repo.authenticate('tlux', 'tlux')
+cdc_data = list(repo['tlux.Raw_CDC_Health'].find())
+with open('C:/Users/tylux/Downloads/cb_2016_25_tract_500k (2).json') as json_file:
+    d = json.load(json_file)
+
+shapes = []
+p = []
+datas = d['features']
+for data in datas:
+    if data['properties']['COUNTYFP'] == '025':
+        geom = data['geometry']
+        p.append(geom)
+g = json.dumps(p)
+with open('C:/Users/tylux/Downloads/boston_census_track.json', 'w') as f:
+    f.write(g)
+
+for data in datas:
+    if data['properties']['COUNTYFP'] == '025':
+        geom = data['geometry']
+        polys = []
+        if geom['type'] == 'Polygon':
+            shape = []
+            coords = geom['coordinates']
+            for i in coords[0]:
+                shape.append((i[0], i[1]))
+            polys.append(Polygon(shape))
+        if geom['type'] == 'MultiPolygon':
+            coords = geom['coordinates']
+            for i in coords:
+                shape = []
+                for j in i:
+                    for k in j:
+                        # need to change list type to tuple so that shapely can read it
+                        shape.append((k[0], k[1]))
+                poly = Polygon(shape)
+                polys.append(poly)
+
+        shapes.append({"Shape":polys, "Census Tract":data['properties']['GEOID']})
+
+print(shapes)
