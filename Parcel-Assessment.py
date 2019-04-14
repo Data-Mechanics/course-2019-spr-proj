3e2e5f6431d5444057a4afd167851f56ed8f8853
ljmcgann <33186994+ljmcgann@users.noreply.github.com>
2019-04-14 15:26:00 -0400

    to be overitten

--- Parcel-Assessment.py
+++ Parcel-Assessment.py
@@ -1,5 +1,7 @@
 import urllib.request
 import json
+from shapely.geometry import Polygon
+
 
 
 #Property assessment data
@@ -27,5 +29,79 @@ for i in Parcels:
             i['PTYPE'] = j["PTYPE"]
             break
 
+
+    #if 'AV_TOTAL' not in i:
+    #    Parcels.remove(i)
+
 #print(Parcels[0])
-#print(json.dumps(Parcels, sort_keys=True, indent = 2))
+
+"""
+for parcel in Parcels:
+    geom = parcel["GEOMETRY"]
+    polys = []
+    if geom['type'] == 'Polygon':
+        shape = []
+        coords = geom['coordinates']
+        for point in coords[0]:
+            shape.append((point[0], point[1]))
+        polys.append(Polygon(shape))
+    if geom['type'] == 'MultiPolygon':
+        coords = geom['coordinates']
+        for i in coords:
+            shape = []
+            for j in i:
+                for k in j:
+                    # need to change list type to tuple so that shapely can read it
+                    shape.append((k[0], k[1]))
+            poly = Polygon(shape)
+            polys.append(poly)
+
+    parcel['SHAPE'] = polys
+    parcel.pop('GEOMETRY', None)
+
+print(json.dumps(Parcels, sort_keys=True, indent = 2))
+
+"""
+"""
+##################################################
+# census track data
+url2 = "http://datamechanics.io/data/gasparde_ljmcgann_tlux/boston_census_track.json"
+response = urllib.request.urlopen(url2).read()
+d = json.loads(response)
+#print(d)
+shapes = []
+p = []
+
+
+for geom in d:
+    polys = []
+    if geom['type'] == 'Polygon':
+        shape = []
+        coords = geom['coordinates']
+        for point in coords[0]:
+            shape.append((point[0], point[1]))
+        polys.append(Polygon(shape))
+    if geom['type'] == 'MultiPolygon':
+        coords = geom['coordinates']
+        for i in coords:
+            shape = []
+            for j in i:
+                for k in j:
+                    # need to change list type to tuple so that shapely can read it
+                    shape.append((k[0], k[1]))
+            poly = Polygon(shape)
+            polys.append(poly)
+
+    shapes.append({"Shape":polys, "Census Tract":geom["Census Tract"]})
+
+print(shapes)
+
+
+url = 'https://chronicdata.cdc.gov/resource/csmm-fdhi.geojson'
+response = urllib.request.urlopen(url).read().decode("utf-8")
+r = json.loads(response)
+s = json.dumps(r, sort_keys=True, indent=2)
+#print(s)
+
+
+"""
