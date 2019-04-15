3dee9737d9385f7bcec9d170d2af76633d223b56
jiaozi1995 <zhou482@purdue.edu>
2019-04-15 18:10:01 -0400

    part 1

--- ruipang_zhou482/README.md
+++ ruipang_zhou482/README.md
@@ -1,1 +1,11 @@
+<Project 1>
 We chose five datasets from Analyse Boston: Property Assessment, Police Station, Public School, Private School and Hospitals. We are trying to develop a relationship between the average house price in each zipcode area and the number of Police station, schools and hospitals in that area. Each dataset will be transformed in the form {"zipcode": xxxxxx, "value": xxxxxx}, where the value is average house price for Property Assessments and number of corresponding facilities in this area. In this form we can find a relationship between housing price and number of public facilities.
+
+
+
+
+<Project 2>
+	Our problem is to find a correlation between Average house price in each zipcode and the number of public facilities in this region (ex. Police Station, School, Hospital). We will divide the problem into 2 parts:
+		1. Develop a linear function to predict average house price using existing data. We solve this problem as a optimization problem. We define our linear function as y=w*x+b where y is the actual price, w is the weight vector, b is the bias and x is the existing data. We define the cost function of our linear function to be MSE = (1/N)∑(yi - (w * xi + b)). In our problem, we want to minimize this cost to get the best linear function to predict average house price. We employ linear regression package from scikit-learn to minimize the cost and derive the linear function for prediction.
+
+		2. Compute the correlation between number of each facility to the housing price. Todo
---
+++ ruipang_zhou482/train.py
@@ -0,0 +1,115 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+import numpy as np
+import os
+from sklearn.linear_model import LinearRegression
+def aggregate(R, f):
+	keys = {r[0] for r in R}
+	return [(key, f([v for (k,v) in R if k == key])) for key in keys]
+class train(dml.Algorithm):
+
+	contributor = 'ruipang_zhou482'
+	reads = ["ruipang_zhou482.hospital", "ruipang_zhou482.police", "ruipang_zhou482.privateSchool", "ruipang_zhou482.publicSchool", "ruipang_zhou482.propertyAssessment"]
+	writes = []
+
+
+
+	@staticmethod
+	def execute(trial = False):
+
+		startTime = datetime.datetime.now()
+
+		client = dml.pymongo.MongoClient()
+		repo = client.repo
+		repo.authenticate('ruipang_zhou482', 'ruipang_zhou482')
+
+		privateSchool = {}
+		for i in repo['ruipang_zhou482.privateSchool'].find():
+			privateSchool[i["zipcode"]] = i["num_school"]
+		publicSchool = {}
+		for i in repo['ruipang_zhou482.publicSchool'].find():
+			publicSchool[i["zipcode"]] = i["num_school"]
+		propertyAssessment = {}
+		for i in repo['ruipang_zhou482.PropertyAssessment'].find():
+			propertyAssessment[i["zipcode"]] = i["avg_value"]
+		police = {}
+		for i in repo['ruipang_zhou482.police'].find():
+			police[i["zipcode"]] = i["count"]
+		hospital = {}
+		for i in repo['ruipang_zhou482.hospital'].find():
+			hospital[i["zipcode"]] = i["count"]
+
+		data = {}
+		for zipcode in propertyAssessment:
+			row = {}
+			if zipcode in publicSchool:
+				row["publicSchool"] = publicSchool[zipcode]
+			else:
+				row["publicSchool"] = 0
+			if zipcode in privateSchool:
+				row["privateSchool"] = privateSchool[zipcode]
+			else:
+				row["privateSchool"] = 0
+			if zipcode in police:
+				row["police"] = police[zipcode]
+			else:
+				row["police"] = 0
+			if zipcode in hospital:
+				row["hospital"] = hospital[zipcode]
+			else:
+				row["hospital"] = 0
+			row["propertyAssessment"] = propertyAssessment[zipcode]
+			data[zipcode] = row
+
+		X = []
+		y = []
+		for zipcode in data:
+			X.append([data[zipcode]["publicSchool"], data[zipcode]["privateSchool"], data[zipcode]["police"], data[zipcode]["hospital"]])
+			y.append(data[zipcode]["propertyAssessment"])
+		print(X, y)
+		reg = LinearRegression().fit(X, y)
+		print(reg.coef_)
+			
+
+		
+		
+		repo.logout()
+
+		endTime = datetime.datetime.now()
+		return {"start": startTime, "end": endTime}
+
+	@staticmethod
+	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+		client = dml.pymongo.MongoClient()
+		repo = client.repo
+		repo.authenticate('ruipang_zhou482', 'ruipang_zhou482')
+
+		doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+		doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+		doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+		doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+		doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+		this_script = doc.agent('alg:train', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+		resource1 = doc.entity('dat:privateSchool', {'prov:label':'privateSchool', prov.model.PROV_TYPE:'ont:DataResource'})
+		resource2 = doc.entity('dat:publicSchool', {'prov:label':'publicSchool', prov.model.PROV_TYPE:'ont:DataResource'})
+		resource3 = doc.entity('dat:police', {'prov:label':'police', prov.model.PROV_TYPE:'ont:DataResource'})
+		resource4 = doc.entity('dat:hospital', {'prov:label':'hospital', prov.model.PROV_TYPE:'ont:DataResource'})
+		resource5 = doc.entity('dat:propertyAssessment', {'prov:label':'propertyAssessment', prov.model.PROV_TYPE:'ont:DataResource'})
+		train_X = doc.entity('dat:train_X', {prov.model.PROV_LABEL:'Training set', prov.model.PROV_TYPE:'ont:DataSet'})
+		train_y = doc.entity('dat:train_y', {prov.model.PROV_LABEL:'Training set', prov.model.PROV_TYPE:'ont:DataSet'})
+
+		doc.wasAttributedTo(train_X, this_script)
+		doc.wasAttributedTo(train_y, this_script)
+		doc.wasDerivedFrom(train_X, resource1, resource2, resource3, resource4)
+		doc.wasDerivedFrom(train_X, resource5)
+
+		repo.logout()
+
+		return doc
+
+train.execute()
