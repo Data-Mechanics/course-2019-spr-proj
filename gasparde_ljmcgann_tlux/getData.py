2e75dcbe69bde73c55297bdb65c51cfedb97ed4e
Tyler Lux <tlux@bu.edu>
2019-04-16 15:03:59 -0400

    moved files so that execute can work

---
+++ gasparde_ljmcgann_tlux/getData.py
@@ -0,0 +1,256 @@
+import urllib.request
+import json
+from shapely.geometry import Polygon
+import json
+import dml
+import prov.model
+import datetime
+import csv
+import codecs
+import uuid
+
+class getData(dml.Algorithm):
+    contributor = 'gasparde_ljmcgann_tlux'
+    reads = []
+    writes = [contributor + ".CensusTractShape", contributor + ".CensusTractHealth",
+              contributor + ".Neighborhoods", contributor + ".ParcelAssessments",
+              contributor + ".ParcelGeo", contributor + ".OpenSpaces"]
+
+    @staticmethod
+    def execute(trial=False):
+        '''Retrieve some data sets '''
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        name = "gasparde_ljmcgann_tlux"
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate(getData.contributor, getData.contributor)
+
+        ######################################################
+
+        # census tract ids with their associated geojson shape
+        url = 'http://datamechanics.io/data/gasparde_ljmcgann_tlux/boston_census_track.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        censusTract = json.loads(response)
+        repo.dropCollection(getData.contributor + ".CensusTractShape")
+        repo.createCollection(getData.contributor + ".CensusTractShape")
+        repo[getData.contributor + ".CensusTractShape"].insert_many(censusTract)
+        repo[getData.contributor + ".CensusTractShape"].metadata({'complete':True})
+
+        #######################################################
+
+        # CDC data of various health metrics within each census tract
+        url = 'https://chronicdata.cdc.gov/resource/47z2-4wuh.json?placename=Boston'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        cdcData = json.loads(response)
+        # print(cdcData)
+        # only want to keep these three statistics
+        for i in range(len(cdcData)):
+            d = {"_id": cdcData[i]["tractfips"],
+                 "obesity": cdcData[i]["obesity_crudeprev"],
+                 "low_phys": cdcData[i]["lpa_crudeprev"],
+                 "asthma": cdcData[i]["casthma_crudeprev"]}
+            cdcData[i] = d
+        repo.dropCollection(getData.contributor + ".CensusTractHealth")
+        repo.createCollection(getData.contributor + ".CensusTractHealth")
+        repo[getData.contributor + ".CensusTractHealth"].insert_many(cdcData)
+        repo[getData.contributor + ".CensusTractHealth"].metadata({'complete': True})
+
+        #########################################################
+
+        # Boston neighborhoods and there associated geojson shapes
+        url = 'http://bostonopendata-boston.opendata.arcgis.com/datasets/3525b0ee6e6b427f9aab5d0a1d0a1a28_0.geojson'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        neighborhoods = json.loads(response)
+        repo.dropCollection(getData.contributor + ".Neighborhoods")
+        repo.createCollection(getData.contributor + ".Neighborhoods")
+        repo[getData.contributor + ".Neighborhoods"].insert_many(neighborhoods["features"])
+        repo[getData.contributor + ".Neighborhoods"].metadata({'complete': True})
+
+        ##########################################################
+
+        #Parcels with their assessment value and type
+        All_Assessments = []
+        # if trial:
+        #     skip = 10000
+        #     url1 = "https://data.boston.gov/datastore/odata3.0/fd351943-c2c6-4630-992d-3f895360febd?$top=2000&$format=json&$skip=" + str(skip)
+        #     response = urllib.request.urlopen(url1).read()
+        #     Assessment = json.loads(response)
+        #     Assessment = Assessment['value']
+        #     All_Assessments += Assessment
+        for i in range(9):
+            # need to iterate because we api request only brings max 20000 parcels
+            skip = 20000 * (i)
+            print(i)
+            url1 = "https://data.boston.gov/datastore/odata3.0/fd351943-c2c6-4630-992d-3f895360febd?$top=20000&$format=json&$skip=" + str(
+                skip)
+            response = urllib.request.urlopen(url1).read()
+            Assessment = json.loads(response)
+            Assessment = Assessment['value']
+            All_Assessments += Assessment
+
+        # this loop is to remove duplicates that would cause problems inserting into mongo
+        ids = set()
+        # print(All_Assessments)
+        unique_pid = []
+        for assess in All_Assessments:
+            if assess["PID"] not in ids:
+                ids.add(assess["PID"])
+                unique_pid.append({"_id":assess["PID"], "AV_TOTAL": assess["AV_TOTAL"], "PTYPE": assess["PTYPE"],
+                                   "LAND_SF":assess["LAND_SF"]})
+
+        #print(unique_pid)
+        repo.dropCollection(getData.contributor + ".ParcelAssessments")
+        repo.createCollection(getData.contributor + ".ParcelAssessments")
+        repo[getData.contributor + ".ParcelAssessments"].insert_many(unique_pid)
+
+        repo[getData.contributor + ".ParcelAssessments"].metadata({'complete': True})
+
+        ##########################################################
+
+        # parcel geojson data
+        url = "http://bostonopendata-boston.opendata.arcgis.com/datasets/b7739e6673104c048f5e2f28bb9b2281_0.geojson"
+
+        response = urllib.request.urlopen(url).read()
+
+        parcelGeo = json.loads(response.decode('utf-8'))["features"]
+        repo.dropCollection(getData.contributor + ".ParcelGeo")
+        repo.createCollection(getData.contributor + ".ParcelGeo")
+        repo[getData.contributor + ".ParcelGeo"].insert_many(parcelGeo)
+        repo[getData.contributor + ".ParcelGeo"].metadata({'complete': True})
+
+        ###########################################################
+
+        # open spaces in boston
+        url = "http://bostonopendata-boston.opendata.arcgis.com/datasets/2868d370c55d4d458d4ae2224ef8cddd_7.geojson"
+        wanted_types = ["Parkways, Reservations & Beaches", "Parks, Playgrounds & Athletic Fields",
+                        "Urban Wilds & Natural Areas", "Community Gardens"]
+        open_spaces = json.loads(urllib.request.urlopen(url).read())["features"]
+        #print(open_spaces)
+        open_spaces = [i for i in open_spaces if i['properties']['TypeLong'] in wanted_types]
+        repo.dropCollection(getData.contributor + ".OpenSpaces")
+        repo.createCollection(getData.contributor + ".OpenSpaces")
+        repo[getData.contributor + ".OpenSpaces"].insert_many(open_spaces)
+        repo[getData.contributor + ".OpenSpaces"].metadata({'complete': True})
+
+        repo.logout()
+
+        endTime = datetime.datetime.now()
+        return {"start": startTime, "end": endTime}
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('gasparde_ljmcgann_tlux', 'gasparde_ljmcgann_tlux')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        this_script = doc.agent('alg:gasparde_ljmcgann_tlux#collect',
+                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'],
+                                 'ont:Extension': 'py'})
+
+        # Data Mechanics Portal
+        doc.add_namespace('bct', 'http://datamechanics.io/data/gasparde_ljmcgann_tlux/')
+
+        bct_resource = doc.entity('bct:boston_census_track',
+                              {'prov:label': 'Collect Boston Census Tract Shapes',
+                                prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+        getCensusTractShape = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(getCensusTractShape, this_script)
+        doc.usage(getCensusTractShape, bct_resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        CensusTractShape = doc.entity('dat:gasparde_ljmcgann_tlux#CensusTractShape',
+                                      {prov.model.PROV_LABEL: 'Boston Census Tract Shape',
+                                       prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(CensusTractShape, this_script)
+        doc.wasGeneratedBy(CensusTractShape, getCensusTractShape, endTime)
+        doc.wasDerivedFrom(CensusTractShape, bct_resource, getCensusTractShape, getCensusTractShape, getCensusTractShape)
+
+
+        # CDC Portal
+        doc.add_namespace('cth','https://chronicdata.cdc.gov/resource/')
+        cth_resource = doc.entity('cth:47z2-4wuh',
+                                  {'prov:label': 'Collect Boston Census Tract Health Statistics',
+                                   prov.model.PROV_TYPE: 'ont:DataResource',
+                                   'ont:Extension': 'json'})
+
+        getCensusTractHealth = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(getCensusTractHealth, this_script)
+        doc.usage(getCensusTractHealth, cth_resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval','ont:Query':'?placename=Boston'})
+
+        CensusTractHealth = doc.entity('dat:gasparde_ljmcgann_tlux#CensusTractHealth',
+                                      {prov.model.PROV_LABEL: 'Boston Census Tract Health Statistics',
+                                       prov.model.PROV_TYPE: 'ont:DataSet'})
+        get_Neighborhoods = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_ParcelAssessments = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_ParcelGeo = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_OpenSpaces = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAttributedTo(CensusTractHealth, this_script)
+        doc.wasGeneratedBy(CensusTractHealth, getCensusTractHealth, endTime)
+        doc.wasDerivedFrom(CensusTractHealth, cth_resource, getCensusTractHealth, getCensusTractHealth,
+                           getCensusTractHealth)
+
+
+
+
+
+        doc.wasAssociatedWith(get_Neighborhoods, this_script)
+        doc.wasAssociatedWith(get_ParcelGeo, this_script)
+        doc.wasAssociatedWith(get_ParcelAssessments, this_script)
+        doc.wasAssociatedWith(get_OpenSpaces, this_script)
+
+
+
+        # doc.usage(get_OpenSpaces, resource, startTime, None,
+        #           {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        # doc.usage(get_ParcelGeo, resource, startTime, None,
+        #           {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        # doc.usage(get_Neighborhoods, resource, startTime, None,
+        #           {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        # doc.usage(get_ParcelAssessments, resource, startTime, None,
+        #           {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        # OpenSpaces = doc.entity('dat:alice_bob#lost',
+        #                   {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
+        # doc.wasAttributedTo(OpenSpaces, this_script)
+        # doc.wasGeneratedBy(OpenSpaces, get_OpenSpaces, endTime)
+        # doc.wasDerivedFrom(OpenSpaces, resource, get_OpenSpaces, get_OpenSpaces, get_OpenSpaces)
+        #
+        # ParcelGeo = doc.entity('dat:alice_bob#found',
+        #                    {prov.model.PROV_LABEL: 'Animals Found', prov.model.PROV_TYPE: 'ont:DataSet'})
+        # doc.wasAttributedTo(ParcelGeo, this_script)
+        # doc.wasGeneratedBy(ParcelGeo, get_ParcelGeo, endTime)
+        # doc.wasDerivedFrom(ParcelGeo, resource, get_ParcelGeo, get_ParcelGeo, get_ParcelGeo)
+        #
+        # Neighborhoods = doc.entity('dat:alice_bob#lost',
+        #                   {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
+        # doc.wasAttributedTo(Neighborhoods, this_script)
+        # doc.wasGeneratedBy(Neighborhoods, get_Neighborhoods, endTime)
+        # doc.wasDerivedFrom(Neighborhoods, resource, get_Neighborhoods, get_Neighborhoods, get_Neighborhoods)
+        #
+        # ParcelAssessments = doc.entity('dat:alice_bob#found',
+        #                    {prov.model.PROV_LABEL: 'Animals Found', prov.model.PROV_TYPE: 'ont:DataSet'})
+        # doc.wasAttributedTo(ParcelAssessments, this_script)
+        # doc.wasGeneratedBy(ParcelAssessments, get_ParcelAssessments, endTime)
+        # doc.wasDerivedFrom(ParcelAssessments, resource, get_ParcelAssessments, get_ParcelAssessments, get_ParcelAssessments)
+
+        repo.logout()
+
+        return doc
+
+
+
+
---
+++ gasparde_ljmcgann_tlux/optimize.py
@@ -0,0 +1,104 @@
+from shapely.geometry import Polygon, Point
+import dml
+import prov.model
+import datetime
+import codecs
+import uuid
+import random
+from scipy.cluster.vq import kmeans
+# import matplotlib.pyplot as pyplt
+
+class optimize(dml.Algorithm):
+    contributor = 'gasparde_ljmcgann_tlux'
+    reads = [contributor + ".Neighborhoods", contributor + ".ParcelsCombined"]
+    writes = [contributor + ".KMeans"]
+
+    @staticmethod
+    def geojson_to_polygon(geom):
+        """
+
+        :return: list of shapely polygons corresponding to the geojson object
+        """
+        polys = []
+        if geom['type'] == 'Polygon':
+            shape = []
+            coords = geom['coordinates']
+            for i in coords[0]:
+                shape.append((i[0], i[1]))
+            polys.append(Polygon(shape))
+        if geom['type'] == 'MultiPolygon':
+            coords = geom['coordinates']
+            for i in coords:
+                shape = []
+                for j in i:
+                    for k in j:
+                        # need to change list type to tuple so that shapely can read it
+                        shape.append((k[0], k[1]))
+                poly = Polygon(shape)
+                polys.append(poly)
+        return polys
+
+
+    @staticmethod
+    def execute(trial=False):
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate(optimize.contributor, optimize.contributor)
+        # read in neighborhoods to get the list of their names
+        neighborhoods = list(repo[optimize.contributor + ".Neighborhoods"].find())
+        # load in parcels as we will iterate kmeans from this data
+        parcels = repo[optimize.contributor + ".ParcelsCombined"]
+
+        repo.dropCollection(optimize.contributor + ".KMeans")
+        repo.createCollection(optimize.contributor + ".KMeans")
+        for i in range(len(neighborhoods)):
+            name = neighborhoods[i]["properties"]["Name"]
+            neighborhood = list(parcels.find({"Neighborhood":name}))
+            print(neighborhood)
+            M = []
+            # x = []
+            # y = []
+
+            for j in range(len(neighborhood)):
+
+                shape = optimize.geojson_to_polygon(neighborhood[j]["geometry"])[0]
+
+                row = [shape.centroid.coords[0][1], shape.centroid.coords[0][0]]
+                # do weighted kmeans by adding additional points
+                weight = max(int(neighborhood[j]["score_improvement"]),1)
+                for _ in range(weight):
+                    M.append([row[0], row[1]])
+
+                    # this was for purpose of making our scatterplots
+                    # look nicer, not needed for kmeans to function properly
+                    # rand = random.random() / 10000
+                    # M.append([row[0] + rand, row[1] + rand])
+                    # x.append(row[0]+ rand)
+                    # y.append(row[1] + rand)
+
+
+                # pyplt.scatter(x,y, s = .5)
+                # means = list(output)[0]
+                # mean_x = []
+                # mean_y = []
+                #
+                # for i in means:
+                #     mean_x.append(i[0])
+                #     mean_y.append(i[1])
+                # pyplt.scatter(mean_x, mean_y)
+                # pyplt.show()
+            if len(M) > 0:
+                output = kmeans(M, 5)[0].tolist()
+                repo[optimize.contributor + ".KMeans"].insert_one({"Neighborhoods": name,"means": output})
+        repo[optimize.contributor + ".KMeans"].metadata({'complete': True})
+
+
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        return 0
+
+
