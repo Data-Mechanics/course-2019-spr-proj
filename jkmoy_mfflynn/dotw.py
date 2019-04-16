361db0499c48b04467009c2e4a99bb311011d010
mfflynn <47292359+mfflynn@users.noreply.github.com>
2019-04-16 16:32:59 -0400

    Update dotw.py

--- jkmoy_mfflynn/dotw.py
+++ jkmoy_mfflynn/dotw.py
@@ -32,7 +32,7 @@ class dotw(dml.Algorithm):
         
         total = len(collection)
         
-        data = [(doc['day_of_week'], 1) for doc in collection if doc['day_of_week'].strip != '']
+        data = [(doc['DAY_OF_WEEK'], 1) for doc in collection if doc['DAY_OF_WEEK'].strip != '']
         data = project(aggregate(data, sum), lambda t: (t[0], t[1], t[1]/500))
 
         finalDataset = [{'dotw':tup[0],'count':tup[1],'percent':tup[2]} for tup in data]
