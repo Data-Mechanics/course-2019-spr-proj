ca4c4a05655195b908525d7f6cbf94e1180987bd
Stephanie Shin <steph.shin2@gmail.com>
2019-04-17 13:58:16 -0400

    spacing issues

--- execute.py
+++ execute.py
@@ -36,7 +36,7 @@ for r,d,f in os.walk(path):
 datasets = set()
 ordered = []
 while len(algorithms) > 0:
-    for i in range(0,len(algorithms)):
+    for i in range(0, len(algorithms)):
         if set(algorithms[i].reads).issubset(datasets):
             datasets = datasets | set(algorithms[i].writes)
             ordered.append(algorithms[i])
