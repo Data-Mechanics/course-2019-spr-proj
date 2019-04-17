5b4842a9d072e11251f71b5eb1126d40b0932a17
runqitian <tianrunqi94@gmail.com>
2019-04-17 13:43:35 -0400

    project2 by runqi tian

--- robinhe_rqtian_hongyf_zhjiang/README.md
+++ robinhe_rqtian_hongyf_zhjiang/README.md
@@ -42,5 +42,75 @@ Datasets may come in as .pdf file, .xlsx file or .txt file. We have convert them
  - Another transformation was an aggregation. This started with taking annual increase in units in different location in Revere. As we ahave no access to the annaully traffic information, the increase in units stands for the traffic of each location. But in this case, we would like to first study the annual change of traffic data. So we aggregate the unit change number by year.
 
  - Our final transformation involved the dataset containing the crash data in several other towns near Revere. With this dataset, we want a town-oriented study and compare the statistics in Revere to that of other surrounding towns. So we first use a projection to cleanup crash Ids as there may exists two different kind of notation. Then we use another projection to put the ID together with crash date, time and most importantly, in which town.
+ 
+ 
+### Supplemental dataset
+We supplement a dataset of car crash spots and visualize it on google map, we will also use this dataset to do our Project2 
 
+
+### Optimization Problem
+
+Problem:
+    We have the dataset of all car-crashing spots in revere city. After using google map to visualize these spots, we found that the car-crashing spots are distributed everywhere in this city. However, we still found several spots in the car accidents heat map where car accidents are most likely to happen, some spots like traffic circle.
+    We believe that there are still other spots which we can not discover simply by observing the heat map of car
+    accidents.So we decided to build up a model about this problem, using the Optimization Techniques to solve
+    this problem.
+
+The problem is about:
+    Find n spots in revere city where car accidents are most likely to happen. In this problem, n is a variable
+    you can choose yourself.
+
+Solution:
+    We abstract this real world problem to a mathematical model. To simplify the problem, we divide the map
+    into grid map and calculate the car accidents happened in each grid. Then we use a slide window to
+    go throw the grids and find the grid where car accidents are most likely to happen. For example n=5, then
+    we have C1,C2,C3,C4,C5 for 5 positions of the slide window.\n
+    System states S = {grid in this grid map}^n\n
+    then the constraints are:
+    1, the grid map is 100*100
+    2, the slide window slides inside the region, the stride is 1 and the window size is 3, it should also be
+    a square window.
+    3, Ci and Cj can not overlap each other, which means if the slide window overlaps, we only keep the bigger one.\n
+    the metric is:
+    1, C = C1 +C2 + ... + C5, we need to find a state in S which maximize C 
+
+Running Example:
+    {'left-up': (42.43524010097626, -71.02176294730468), 'right-up': (42.43524010097626, -71.01961702927072), 'right-bottom': (42.43691010921584, -71.01961702927072), 'left-bottom': (42.43691010921584, 42.43524010097626)}
+    {'left-up': (42.40684996090348, -70.99315070685178), 'right-up': (42.40684996090348, -70.99100478881782), 'right-bottom': (42.40851996914305, -70.99100478881782), 'left-bottom': (42.40851996914305, 42.40684996090348)}
+    {'left-up': (42.429673406844344, -71.01961702927072), 'right-up': (42.429673406844344, -71.01747111123674), 'right-bottom': (42.431343415083916, -71.01747111123674), 'left-bottom': (42.431343415083916, 42.429673406844344)}
+    {'left-up': (42.40851996914305, -71.0031649910103), 'right-up': (42.40851996914305, -71.00101907297633), 'right-bottom': (42.41018997738263, -71.00101907297633), 'left-bottom': (42.41018997738263, 42.40851996914305)}
+    {'left-up': (42.42076669623327, -71.00602621505558), 'right-up': (42.42076669623327, -71.00388029702162), 'right-bottom': (42.42243670447285, -71.00388029702162), 'left-bottom': (42.42243670447285, 42.42076669623327)}
+    These are top 5 areas where car accidents are most likely to happen.
+    
+    
+### Statistics:
+Problem:
+    The number of injuries depends on the severity of the crash. Here in the dataset, we have columns of data about
+    injuries in every accident, non-fatal and fatal ones. We also have columns about number of cars involved in the
+    accident. We want to find out if there is any relation between them.
+
+Problem:
+    the relation between the number of cars involved in the accident and the number of injuries.
+
+Solution:
+    1, calculate average and standard deviation of non-fatal injuries, fatal injuries, injuries and number of cars.
+    2, calculate correlation coefficient between these dimensions.
+
+Running Examples:
+    'avg_non_fatal': 0.41782467236293974, 
+    'avg_fatal': 0.00246297929272965, 
+    'avg_injuries': 0.4202876516556694, 
+    'avg_car': 1.9341077021315414, 
+    'std_non_fatal': 0.7734077132371413, 
+    'std_fatal': 0.054253363526365506, 
+    'std_injuries': 0.7763725934315647, 
+    'std_car': 0.626645342789055, 
+    'cov_nonfatal_car': 0.025190077082430692, 
+    'cov_fatal_car': -0.0007803303394241795, 
+    'cov_injuries_car': 0.024409746743014386, 
+    'corr_nonfatal_car': 0.05197556046145466, 
+    'corr_fatal_car': -0.022952500641731856, 
+    'corr_injuries_car': 0.050173137652113155
+    
+    
 **The right to use these data is granted**
---
+++ robinhe_rqtian_hongyf_zhjiang/Statistics.py
@@ -0,0 +1,181 @@
+'''
+Problem:
+    The number of injuries depends on the severity of the crash. Here in the dataset, we have columns of data about
+    injuries in every accident, non-fatal and fatal ones. We also have columns about number of cars involved in the
+    accident. We want to find out if there is any relation between them.
+
+    Problem:
+        the relation between the number of cars involved in the accident and the number of injuries.
+
+    Solution:
+        1, calculate average and standard deviation of non-fatal injuries, fatal injuries, injuries and number of cars.
+        2, calculate correlation coefficient between these dimensions.
+
+'''
+import numpy as np
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+
+
+class statistics(dml.Algorithm):
+    contributor = 'robinhe_rqtian_hongyf_zhjiang'
+    reads = []
+    writes = ['robinhe_rqtian_hongyf_zhjiang.statistics']
+
+    @staticmethod
+    def average(arr):
+        sum = 0
+        count = 0
+        for item in arr:
+            count += 1
+            sum += item
+        # print(sum,count)
+        avg = sum/count
+        return avg
+
+
+    @staticmethod
+    def standard_deviation(arr):
+        avg = statistics.average(arr)
+        sum = 0
+        count = 0
+        for item in arr:
+            count += 1
+            sum += np.square(item - avg)
+        std = np.sqrt(sum/count)
+        return std
+
+    @staticmethod
+    def covariance(x,y):
+        avg_x = statistics.average(x)
+        avg_y = statistics.average(y)
+        return sum([(xi - avg_x) * (yi - avg_y) for (xi, yi) in zip(x, y)]) / len(x)
+
+    @staticmethod
+    def corr(x, y):  # Correlation coefficient.
+        if statistics.standard_deviation(x) * statistics.standard_deviation(y) != 0:
+            return statistics.covariance(x, y) / (statistics.standard_deviation(x) * statistics.standard_deviation(y))
+
+    @staticmethod
+    def execute(trial=False):
+        '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('robinhe_rqtian_hongyf_zhjiang', 'robinhe_rqtian_hongyf_zhjiang')
+
+        url = 'http://datamechanics.io/data/robinhe_rqtian_hongyf_zhjiang/crash_data_01_19.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+
+        output = []
+        inter = []
+        for item in r:
+            try:
+                inter.append([int(item['Number of NonFatal Injuries']),
+                int(item['Number of Fatal Injuries']),
+                int(item['Number of NonFatal Injuries'] + item['Number of Fatal Injuries']),
+                int(item['Number of Vehicles'])])
+            except ValueError:
+                continue
+        inter = np.array(inter)
+        print(inter.shape)
+        avg_non_fatal = statistics.average(inter[:, 0])
+        avg_fatal = statistics.average(inter[:, 1])
+        avg_injuries = statistics.average(inter[:, 2])
+        avg_car = statistics.average(inter[:, 3])
+        std_non_fatal = statistics.standard_deviation(inter[:, 0])
+        std_fatal = statistics.standard_deviation(inter[:, 1])
+        std_injuries = statistics.standard_deviation(inter[:, 2])
+        std_car = statistics.standard_deviation(inter[:, 3])
+        cov_nonfatal_car = statistics.covariance(inter[:, 0],inter[:,3])
+        cov_fatal_car = statistics.covariance(inter[:, 1], inter[:,3])
+        cov_injuries_car = statistics.covariance(inter[:,2], inter[:,3])
+        corr_nonfatal_car = statistics.corr(inter[:, 0],inter[:,3])
+        corr_fatal_car = statistics.corr(inter[:, 1], inter[:,3])
+        corr_injuries_car = statistics.corr(inter[:,2], inter[:,3])
+
+        output.append({'avg_non_fatal':avg_non_fatal,
+                       'avg_fatal':avg_fatal,
+                       'avg_injuries':avg_injuries,
+                       'avg_car':avg_car,
+                       'std_non_fatal':std_non_fatal,
+                       'std_fatal':std_fatal,
+                       'std_injuries':std_injuries,
+                       'std_car':std_car,
+                       'cov_nonfatal_car':cov_nonfatal_car,
+                       'cov_fatal_car':cov_fatal_car,
+                       'cov_injuries_car':cov_injuries_car,
+                       'corr_nonfatal_car':corr_nonfatal_car,
+                       'corr_fatal_car':corr_fatal_car,
+                       'corr_injuries_car':corr_injuries_car})
+        s = json.dumps(output, sort_keys=True, indent=2)
+        print(output)
+        repo.dropCollection("statistics")
+        repo.createCollection("statistics")
+        repo['robinhe_rqtian_hongyf_zhjiang.statistics'].insert_many(output)
+        repo['robinhe_rqtian_hongyf_zhjiang.statistics'].metadata({'complete': True})
+        print(repo['robinhe_rqtian_hongyf_zhjiang.statistics'].metadata())
+        repo.logout()
+
+        endTime = datetime.datetime.now()
+
+        return {"start": startTime, "end": endTime}
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        '''
+            Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.
+            '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('robinhe_rqtian_hongyf_zhjiang', 'robinhe_rqtian_hongyf_zhjiang')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont',
+                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+        this_script = doc.agent('alg:robinhe_rqtian_hongyf_zhjiang#statistics',
+                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('bdp:wc8w-nujj',
+                              {'prov:label': 'crash accidents data', prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+        get_statistics = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_statistics, this_script)
+        doc.usage(get_statistics, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval',
+                   'ont:Query': '?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+                   }
+                  )
+
+        statistics_injury_car = doc.entity('dat:robinhe_rqtian_hongyf_zhjiang#statistics',
+                          {prov.model.PROV_LABEL: 'statistics of relevance between car number and injury number', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(statistics_injury_car, this_script)
+        doc.wasGeneratedBy(statistics_injury_car, get_statistics, endTime)
+        doc.wasDerivedFrom(statistics_injury_car, resource, get_statistics, get_statistics, get_statistics)
+
+        repo.logout()
+
+        return doc
+
+
+# This is example code you might use for debugging this module.
+# Please remove all top-level function calls before submitting.
+# statistics.execute()
+# doc = statistics.provenance()
+# print(doc.get_provn())
+# print(json.dumps(json.loads(doc.serialize()), indent=4))
+
+## eof
