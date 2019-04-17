0e1613550dfb87b5624275b6234cb63b39fea0b0
misn15 <misn15@bu.edu>
2019-04-17 02:36:09 -0400

    added logreg

--- misn15/linearRegression.py
+++ misn15/linearRegression.py
@@ -134,23 +134,28 @@ class linearRegression(dml.Algorithm):
             in this script. Each run of the script will generate a new
             document describing that invocation event.
             '''
-        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/misn15/') # The scripts are in <folder>#<filename> format.
-        doc.add_namespace('dat', 'http://datamechanics.io/data/misn15/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         
-        this_script = doc.agent('alg:linearRegression', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('dat:crime_health_waste_space', {'prov:label':'Boston Crime, Health, Waste and Open Space Data', prov.model.PROV_TYPE:'ont:DataResource'})
+        this_script = doc.agent('alg:misn15#linearRegression', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('dat:misn15#crime_health_waste_space', {'prov:label':'Boston Crime, Health, Waste and Open Space Data', prov.model.PROV_TYPE:'ont:DataResource'})
         this_run = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         doc.wasAssociatedWith(this_run, this_script)
         doc.usage(this_run, resource, startTime, None,
                   {prov.model.PROV_TYPE:'ont:Retrieval'
                         }
                   )
-        resource2 = doc.entity('dat:reg_results', {prov.model.PROV_LABEL:'Linear Regression Results', prov.model.PROV_TYPE:'ont:DataSet'})
+        resource2 = doc.entity('dat:misn15#reg_results', {prov.model.PROV_LABEL:'Linear Regression Results', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(resource2, this_script)
         doc.wasGeneratedBy(resource2, this_run, endTime)
         doc.wasDerivedFrom(resource2, resource, this_run, this_run, this_run)
+
+        resource3 = doc.entity('dat:misn15#log_results', {prov.model.PROV_LABEL: 'Logistic Regression Results', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(resource3, this_script)
+        doc.wasGeneratedBy(resource3, this_run, endTime)
+        doc.wasDerivedFrom(resource3, resource, this_run, this_run, this_run)
 
         return doc
 
