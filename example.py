065a8d265ee5d66da3c0c17e209a4a9d5e26087e
Stephanie Shin <steph.shin2@gmail.com>
2019-04-17 13:48:42 -0400

    spacing issues

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ class example(dml.Algorithm):
     writes = ['alice_bob.lost', 'alice_bob.found']
 
     @staticmethod
-    def execute(trial=False):
+    def execute(trial = False):
         '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
         startTime = datetime.datetime.now()
 
@@ -28,7 +28,7 @@ class example(dml.Algorithm):
         repo.dropCollection("lost")
         repo.createCollection("lost")
         repo['alice_bob.lost'].insert_many(r)
-        repo['alice_bob.lost'].metadata({'complete': True})
+        repo['alice_bob.lost'].metadata({'complete':True})
         print(repo['alice_bob.lost'].metadata())
 
         url = 'http://cs-people.bu.edu/lapets/591/examples/found.json'
@@ -46,7 +46,7 @@ class example(dml.Algorithm):
         return {"start": startTime, "end": endTime}
 
     @staticmethod
-    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+    def provenance(doc = prov.model.ProvDocument(), startTime= None, endTime= None):
         '''
             Create the provenance document describing everything happening
             in this script. Each run of the script will generate a new
@@ -59,16 +59,12 @@ class example(dml.Algorithm):
         repo.authenticate('alice_bob', 'alice_bob')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
-        doc.add_namespace('ont',
-                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:alice_bob#example',
-                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
-        resource = doc.entity('bdp:wc8w-nujj',
-                              {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
-                               'ont:Extension': 'json'})
+        this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('bdp:wc8w-nujj', {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource', 'ont:Extension': 'json'})
         get_found = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
         get_lost = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
         doc.wasAssociatedWith(get_found, this_script)
@@ -84,14 +80,12 @@ class example(dml.Algorithm):
                    }
                   )
 
-        lost = doc.entity('dat:alice_bob#lost',
-                          {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
+        lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(lost, this_script)
         doc.wasGeneratedBy(lost, get_lost, endTime)
         doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
 
-        found = doc.entity('dat:alice_bob#found',
-                           {prov.model.PROV_LABEL: 'Animals Found', prov.model.PROV_TYPE: 'ont:DataSet'})
+        found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL: 'Animals Found', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(found, this_script)
         doc.wasGeneratedBy(found, get_found, endTime)
         doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
@@ -107,5 +101,6 @@ example.execute()
 doc = example.provenance()
 print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))
+
 
 ## eof
