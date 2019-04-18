b4b125a55382d3dab7cace0c94dea509e56b1312
hkennedy <hek@commonwealthcrypto.com>
2019-04-17 21:46:30 -0400

    trial mode; individual district clusters

--- hek_kquirk/boston_crime_clusters.py
+++ hek_kquirk/boston_crime_clusters.py
@@ -12,13 +12,24 @@ from scipy.cluster.vq import vq, kmeans, whiten
 def goodCoords(t):
     return abs(t[0]) > 0.0 and t[0] != -1 and abs(t[1]) > 0.0 and t[1] != -1
 
+stations = {'A1': (42.361751,-71.060117),
+            'A15': (42.361751,-71.060117),
+            'A7': (42.3710345,-71.0388061),
+            'B2': (42.3284313,-71.0862445),
+            'B3': (42.2846826,-71.0916905),
+            'C6': (42.361751,-71.060117),
+            'C11': (42.2980276,-71.0590886),
+            'D4': (42.3394369,-71.0692247),
+            'D14': (42.3493611,-71.1505902),
+            'E5': (42.2867829,-71.1484246),
+            'E13': (42.299275,-71.115235),
+            'E18': (42.2564288,-71.1242782)}
+
 class boston_crime_clusters(dml.Algorithm):
     contributor = 'hek_kquirk'
     reads = ['hek_kquirk.boston_crime_incidents']
     writes = ['hek_kquirk.boston_crime_clusters']
 
-    stations = np.array([(42.361751,-71.060117),(42.3710345,-71.0388061),(42.3284313,-71.0862445),(42.2846826,-71.0916905),(42.361751,-71.060117),(42.2980276,-71.0590886),(42.3394369,-71.0692247),(42.3493611,-71.1505902),(42.2867829,-71.1484246),(42.299275,-71.115235),(42.2564288,-71.1242782)])
-    
     @staticmethod
     def execute(trial = False):
         startTime = datetime.datetime.now()
@@ -33,30 +44,45 @@ class boston_crime_clusters(dml.Algorithm):
         repo.createCollection("boston_crime_clusters")
 
         # Get crime incident locations and convert to float tuples
-        incident_locs_raw = repo['hek_kquirk.boston_crime_incidents'].find({}, {'Location': 1})
-        incident_locs = [literal_eval(inc['Location']) for inc in incident_locs_raw
-                               if goodCoords(literal_eval(inc['Location']))]
-
-        #One cluster per police station
-        incident_locs_np = np.array(incident_locs)
-        res = kmeans(incident_locs_np, boston_crime_clusters.stations)
+        incident_locs_raw = repo['hek_kquirk.boston_crime_incidents'].find({}, {'Location': 1, 'DISTRICT': 1})
+        if trial:
+            incident_locs_raw = incident_locs_raw[:1000]
         
-        #Create kml file (for google maps)
-        d = ''
-        d += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-        d += "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n"
-        d += "<Document>\n"
-        for r in res[0]:
-            d += "<Placemark>\n"
-            d += "<Point>\n"
-            d += "<coordinates>" + str(r[1]) + "," + str(r[0]) + ",0</coordinates>"
-            d += "</Point>\n"
-            d += "</Placemark>\n"
-        d += "</Document>\n"
-        d += "</kml>"
+        locs_dict = {}
+        for k in stations:
+            locs_dict[k] = []
 
-        doc = {"_id": "boston_crime_clusters.kml", "contents": d}
-        repo['hek_kquirk.boston_crime_clusters'].insert(doc)
+        #Sort coordinates by district into locs_dict
+        for l, s in ((literal_eval(inc['Location']), inc['DISTRICT']) for inc in incident_locs_raw):
+            if goodCoords(l) and s in stations:
+                locs_dict[s].append(l)
+        
+        results = dict.fromkeys(k for k in stations)
+        
+        #Run kmeans and obtain one cluster per police station
+        for k in locs_dict:
+            incident_locs_np = np.array(locs_dict[k])
+            results[k] = kmeans(incident_locs_np, 1)
+
+        with open('clusters.kml', 'w') as d:
+            #Create kml file (for google maps)
+            d.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+            d.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n")
+            d.write("<Document>\n")
+            for k in results:
+                d.write("<Placemark>\n")
+                d.write("<Point>\n")
+                d.write("<coordinates>" + str(results[k][0][0][1]) + "," + str(results[k][0][0][0]) + ",0</coordinates>")
+                d.write("</Point>\n")
+                d.write("</Placemark>\n")
+            d.write("</Document>\n")
+            d.write("</kml>")
+
+        #Write KML file to database
+        with open('clusters.kml', 'r') as f:
+            d = f.read()
+            doc = {"_id": "boston_crime_clusters.kml", "contents": d}
+            repo['hek_kquirk.boston_crime_clusters'].insert(doc)
 
         repo['hek_kquirk.boston_crime_clusters'].metadata({'complete':True})
         print(repo['hek_kquirk.boston_crime_clusters'].metadata())
@@ -74,7 +100,6 @@ class boston_crime_clusters(dml.Algorithm):
             in this script. Each run of the script will generate a new
             document describing that invocation event.
             '''
-
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
@@ -83,6 +108,22 @@ class boston_crime_clusters(dml.Algorithm):
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        
+        this_script = doc.agent('alg:hek_kquirk#boston_crime_clusters', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        
+        resource = doc.entity('dat:hek_kquirk#boston_crime_incidents', {'prov:label':'Boston Crime Incidents', prov.model.PROV_TYPE:'ont:DataResource'})
+        get_boston_crime_clusters = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_boston_crime_clusters, this_script)
+        doc.usage(get_boston_crime_clusters, resource, startTime, None,
+                  {
+                      prov.model.PROV_TYPE:'ont:Computation'
+                  }
+        )
+        
+        boston_crime_clusters = doc.entity('dat:hek_kquirk#boston_crime_clusters', {prov.model.PROV_LABEL:'Boston Crime Clusters', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(boston_crime_clusters, this_script)
+        doc.wasGeneratedBy(boston_crime_clusters, get_boston_crime_clusters, endTime)
+        doc.wasDerivedFrom(boston_crime_clusters, resource, get_boston_crime_clusters, get_boston_crime_clusters, get_boston_crime_clusters)
 
         repo.logout()
         
