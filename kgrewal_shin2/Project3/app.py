b99c34bf83bb1a2518503b11999415f3e6b34ca5
Kerin Grewal <kgrewal@bu.edu>
2019-04-27 02:05:14 -0400

    made this app bop

--- kgrewal_shin2/Project3/app.py
+++ kgrewal_shin2/Project3/app.py
@@ -14,14 +14,14 @@ GoogleMaps(app)
 
 @app.route('/')
 def mapview():
+    return render_template("about.html")
 
+
+@app.route('/map')
+def about():
     url = "https://maps.googleapis.com/maps/api/js?key=" + getGoogleMapsKey() + "&libraries=places&callback=initMap"
 
     return render_template("index.html", url=url)
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
 
 @app.route('/neighborhood', methods=['GET', 'POST'])
 def neighborhood():
@@ -39,11 +39,37 @@ def neighborhood():
         for s in neigh_streets:
             if s['neighborhood'] == nhood and s['gender']=='male':
                 streets.append(s['full_name'])
-
+        if len(streets) == 0:
+            streets.append("No Street Suggestions Found")
         repo.logout()
 
-
     return render_template("search.html", url=url, neighborhood=nhood, streets=streets)
+
+
+@app.route('/street', methods=['GET', 'POST'])
+def street():
+    result = flask.request.form
+    nhood = result['neighborhood']
+    street = result['street']
+    print(nhood)
+    print(street)
+    url = "https://maps.googleapis.com/maps/api/js?key=" + getGoogleMapsKey() + "&libraries=places&callback=initMap"
+
+    if request.method == 'POST':
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('kgrewal_shin2', 'kgrewal_shin2')
+        neigh_streets = list(repo['kgrewal_shin2.neigh_streets'].find())
+
+        streets = []
+        for s in neigh_streets:
+            if s['neighborhood'] == nhood and s['gender'] == 'male':
+                streets.append(s['full_name'])
+        if len(streets) == 0:
+            streets.append("No Street Suggestions Found")
+        repo.logout()
+
+    return render_template("stsearch.html", url=url, street=street, neighborhood=nhood, streets=streets)
 
 
 if __name__ == '__main__':
