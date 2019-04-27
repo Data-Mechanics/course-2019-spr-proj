3cfde304112028a567e8d89e17e0c0a08551e48f
MushroomLin <oneera.mushroom@gmail.com>
2019-04-27 17:33:52 -0400

    project3 structure

--- liweixi_mogujzhu/Project3/server.py
+++ liweixi_mogujzhu/Project3/server.py
@@ -1,54 +1,78 @@
 from tqdm import tqdm
 import json
 import jsonschema
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, send_from_directory
 import pymongo
+import dml
+# The project structure references https://github.com/Data-Mechanics/course-2018-spr-proj/tree/master/agoncharova_lmckone
 
-nations = json.load(open("nations.json"))["nations"]
 
-def interpolateData(year):
-    return [{
-        "name": d["name"],
-        "region": d["region"],
-        "income": interpolateValues(d["income"], year),
-        "population": interpolateValues(d["population"], year),
-        "lifeExpectancy": interpolateValues(d["lifeExpectancy"], year)
-      } for d in nations]
 
-def interpolateValues(values, year):
-    ys = [y for (y,v) in values if y >= year]
-    i = [y for (y,v) in values].index(ys[0])
-    year_next = values[i][0]
-    data_next = values[i][1]
-    if i == 0:
-        return data_next;
-    elif i > 0:
-        year_prev = values[i-1][0]
-        data_prev = values[i-1][1]
-        a = (year - year_next) / (year_prev - year_next)
-        return data_next*(1-a) + data_prev*a
+def generate_xxx(year):
+    return []
 
-print("Populating database...")
-client = pymongo.MongoClient()
-db = client.local
-db.nations.remove({})
-for year in tqdm(range(1800,2006)):
-    db.nations.insert({"_id": year, "data": interpolateData(year)})
-    
+def get_xxx_from_database():
+  client = dml.pymongo.MongoClient()
+  repo = client.repo
+  repo.authenticate('liweixi_mogujzhu', 'liweixi_mogujzhu')
+  xxx_collection = repo['liweixi_mogujzhu.xxx']
+  xxx = xxx_collection.find()
+  evictions_arr = []
+  years = []
+  count = 0
+  # TODO Retrive the data
+
+
+
+  # format it in the valid format to serve to the map
+  repo.logout()
+  print("Formatted " + str(count) + " xxx")
+  return []
+
+def generate_xxx_json_file():
+  '''
+  Writes formatted evictions to the file
+  '''
+  print("Write xxx to a json file")
+  xxx_file = open("./data/xxx.json","w")
+  xxx_file.write('var xxx_json = {')
+  xxx_file.write('"type": "FeatureCollection",')
+  xxx_file.write('"features":')
+
+  xxx = get_xxx_from_database()
+  xxx_file.write(json.dumps(xxx))
+  xxx_file.write("}")
+
+
 app = Flask(__name__)
 
+# serve data
+@app.route('/data/<path:path>')
+def serve_data(path):
+  # get the evictions data from the database and 
+  # generate the json file
+  print(path + " requested")
+  if(path == "xxx.json"):
+    generate_xxx_json_file()
+    print("Generated xxx json file for the map")
+  if(path == "xxx2.json"):
+    generate_xxx_json_file()
+    print("Generated xxx2 json file for the graph")
+  return send_from_directory('./data', path)
+
+# main css file
+@app.route('/css/<path:path>')
+def serve_css(path):
+  return send_from_directory('./css', path)
+
+# javascript files
+@app.route('/js/<path:path>')
+def serve_js(path):
+  return send_from_directory('./js', path)
+
 @app.route('/', methods=['GET'])
-def get_client():
-    return open('nations.html','r').read()
-
-@app.route('/nations.js', methods=['GET'])
-def get_nations():
-    return open('nations.js','r').read()
-
-@app.route('/year/<int:year>', methods=['GET'])
-def get_year(year):
-    for result in db.nations.find({"_id":year}):
-        return jsonify(result["data"])
+def get_index_page():
+    return open('./html/index.html','r').read()
 
 @app.errorhandler(404)
 def not_found(error):
