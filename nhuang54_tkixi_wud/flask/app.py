4b532be592ce9f43d75995e9e06cf5ea3ebc7ce9
Tkixi <tkixi@bu.edu>
2019-04-28 13:00:58 -0400

    cleaned up

--- nhuang54_tkixi_wud/flask/app.py
+++ nhuang54_tkixi_wud/flask/app.py
@@ -13,26 +13,3 @@ def map():
 @app.route('/analysis', methods=["GET", "POST"])
 def analyze():
   return render_template('analysis.html')
-
-@app.route('/visualize', methods=['POST'])
-def visualize():
-	month1 = request.form['month1']
-	month2 = request.form['month2']
-	month3 = request.form['month3']
-	month4 = request.form['month4']
-	month5 = request.form['month5']
-	month6 = request.form['month6']
-	month7 = request.form['month7']
-	month8 = request.form['month8']
-	month9 = request.form['month9']
-	month10 = request.form['month10']
-	month11 = request.form['month11']
-	month12 = request.form['month12']
-
-	# http://datamechanics.io/data/boston_collisions.json
-
-	## pull all the collisions of the particular months (lat,lng in list)
-	## pass that lat lng to the template
-	## template would then load all the lat lng to the map
-
-	return render_template('analysis.html', plots)
