9ec18c90a2dacf0888c1e4194fa8f4fa699bcd83
Ivorine <ido@bu.edu>
2019-04-30 17:35:12 -0400

    web update

--- ido_jconstan_jeansolo_suitcase/visualizations/scripts.js
+++ ido_jconstan_jeansolo_suitcase/visualizations/scripts.js
@@ -13,29 +13,52 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+  // // Begin accessing JSON data here
+  // var data = JSON.parse(this.response);
+  // if (request.status >= 200 && request.status < 400) {
+  //   data.forEach(movie => {
+  //     const card = document.createElement('div');
+  //     card.setAttribute('class', 'card');
 
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+  //     const h1 = document.createElement('h1');
+  //     h1.textContent = movie.title;
 
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+  //     const p = document.createElement('p');
+  //     movie.description = movie.description.substring(0, 300);
+  //     p.textContent = `${movie.description}...`;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
+  //     container.appendChild(card);
+  //     card.appendChild(h1);
+  //     card.appendChild(p);
+  //   });
+  // } else {
+  //   const errorMessage = document.createElement('marquee');
+  //   errorMessage.textContent = `Gah, it's not working!`;
+  //   app.appendChild(errorMessage);
+
+  // 1. Create the button
+  
+  var buttonBefore = document.createElement("button");
+  buttonBefore.innerHTML = "Bus Stops Before";
+  buttonBefore.setAttribute('class', 'card');
+
+  var buttonAfter = document.createElement("button");
+  buttonAfter.innerHTML = "Bus Stops After";
+  buttonAfter.setAttribute('class', 'card');
+
+  // 2. Append somewhere
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(buttonBefore);
+  body.appendChild(buttonAfter);
+
+  // 3. Add event handler
+  buttonBefore.addEventListener ("click", function() {
+    window.location.href = "mapBefore.html";
+  });
+  buttonAfter.addEventListener ("click", function() {
+    window.location.href = "mapAfter.html";
+  });
+  
   }
-}
 
 request.send();
