5bd7990fc6eedaba05834a6646f5002099c315d6
darren68 <47162432+darren68@users.noreply.github.com>
2019-05-03 22:21:12 -0400

    Update Clustering.py

--- darren68_gladding_ralcalde/Clustering.py
+++ darren68_gladding_ralcalde/Clustering.py
@@ -189,17 +189,17 @@ class Clustering(dml.Algorithm):
             count = 0
             for doc in docs:
                 count += 1
-                if len(doc['x']) != 0 and len(doc['y']) != 0 and ((count % 20) == 0) and doc['x'] != "0" and doc['y'] != "0":
+                if len(doc['x']) != 0 and len(doc['y']) != 0 and ((count % 10) == 0):
                     liOfXs.append(float(doc['x']))
                     liOfYs.append(float(doc['y']))
-                    liOfDates.append(doc['crashdate'][-4:])
+                    liOfDates.append(str(doc['datetime'].year))
                     liOfCIDs.append(doc['crashnumber'])
         else:
             for doc in docs:
-                if len(doc['x']) != 0 and (len(doc['y']) != 0) and doc['x'] != "0" and doc['y'] != "0":
+                if len(doc['x']) != 0 and len(doc['y']) != 0:
                     liOfXs.append(float(doc['x']))
                     liOfYs.append(float(doc['y']))
-                    liOfDates.append(doc['crashdate'][-4:])
+                    liOfDates.append(str(doc['datetime'].year))
                     liOfCIDs.append(doc['crashnumber'])
 
         # All the data points are here, I should save them into mongo
@@ -472,3 +472,4 @@ class Clustering(dml.Algorithm):
 
 
         return doc
+
