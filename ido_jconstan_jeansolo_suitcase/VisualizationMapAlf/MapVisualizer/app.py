0c4a2ef37d0c57f4f39138576eeab4c2664ba34c
Alfredo Garcia <suitcase@bu.edu>
2019-05-05 23:48:03 -0400

    oh my god

--- ido_jconstan_jeansolo_suitcase/VisualizationMapAlf/MapVisualizer/app.py
+++ ido_jconstan_jeansolo_suitcase/VisualizationMapAlf/MapVisualizer/app.py
@@ -4,8 +4,10 @@ import gmplot
 import json
 import uuid
 import urllib.request
+import os
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 contributor = 'ido_jconstan_jeansolo_suitcase'
 
 #default page  
@@ -17,6 +19,7 @@ def hello():
 	gmapEmpty.apikey = 'AIzaSyAePvzBOkxdh5YYcgTQjhY9bHWlNEn3Sog'
 
 	gmapEmpty.draw( "templates\\dispmap.html" ) 
+	gmapEmpty.draw( "static\\dispmap.html" ) 
 
 	return render_template('busstops.html', name="hello")
 
@@ -50,7 +53,8 @@ def updateMapBefore():
 
 	gmapBefore.apikey = 'AIzaSyAePvzBOkxdh5YYcgTQjhY9bHWlNEn3Sog'
 
-	gmapBefore.draw('static/dispmap.html' ) 
+	gmapBefore.draw('static\\dispmap.html' ) 
+	gmapBefore.draw('templates\\dispmap.html' )
 
 	return render_template('busstops.html', name="test")
 
@@ -344,9 +348,34 @@ def updateMapAfter():
 
 	gmapAfter.apikey = 'AIzaSyAePvzBOkxdh5YYcgTQjhY9bHWlNEn3Sog'
 
-	gmapAfter.draw( "static\dispmap.html" ) 
+	gmapAfter.draw( "static\\dispmap.html" ) 
+	gmapAfter.draw( "templates\\dispmap.html" ) 
 
 	return render_template('busstops.html', name="test")
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 def union(R, S):
     return R + S
