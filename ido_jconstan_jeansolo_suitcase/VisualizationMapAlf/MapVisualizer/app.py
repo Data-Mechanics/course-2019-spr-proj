0da5ab6ee5e45372c74b0ad485538fb6ee7b3cfd
Alfredo Garcia <suitcase@bu.edu>
2019-05-06 13:52:37 -0400

    final button

--- ido_jconstan_jeansolo_suitcase/VisualizationMapAlf/MapVisualizer/app.py
+++ ido_jconstan_jeansolo_suitcase/VisualizationMapAlf/MapVisualizer/app.py
@@ -329,22 +329,22 @@ def updateMapAfter():
 	after_size = 40
 
 	# scatter points on the google map 
-	gmapAfter.scatter( latitude_list_k0, longitude_list_k0, '# FF0000', 
+	gmapAfter.scatter( latitude_list_k0, longitude_list_k0, '#FF0000', 
 	                              size = after_size, marker = False ) 
 	# scatter points on the google map 
-	gmapAfter.scatter( latitude_list_k1, longitude_list_k1, '# FF0000', 
+	gmapAfter.scatter( latitude_list_k1, longitude_list_k1, '#FF0000', 
 	                              size = after_size, marker = False ) 
 	# scatter points on the google map 
-	gmapAfter.scatter( latitude_list_k2, longitude_list_k2, '# FF0000', 
+	gmapAfter.scatter( latitude_list_k2, longitude_list_k2, '#FF0000', 
 	                              size = after_size, marker = False ) 
 	# scatter points on the google map 
-	gmapAfter.scatter( latitude_list_k3, longitude_list_k3, '# FF0000', 
+	gmapAfter.scatter( latitude_list_k3, longitude_list_k3, '#FF0000', 
 	                              size = after_size, marker = False ) 
 	# scatter points on the google map 
-	gmapAfter.scatter( latitude_list_k4, longitude_list_k4, '# FF0000', 
+	gmapAfter.scatter( latitude_list_k4, longitude_list_k4, '#FF0000', 
 	                              size = after_size, marker = False ) 
 	# scatter points on the google map 
-	gmapAfter.scatter( latitude_list_k5, longitude_list_k5, '# FF0000', 
+	gmapAfter.scatter( latitude_list_k5, longitude_list_k5, '#FF0000', 
 	                              size = after_size, marker = False ) 
 
 	gmapAfter.apikey = 'AIzaSyAePvzBOkxdh5YYcgTQjhY9bHWlNEn3Sog'
@@ -366,6 +366,327 @@ def resetMap():
 
 
 	return render_template('busstops.html', name="test")
+
+@app.route("/updateMapBoth", methods=['GET', 'POST'])
+def updateMapBoth():
+	# DATA SET 9 [StopsLatLng]
+	# Bus Stops Latitude and Longitude
+	# r9 = {'lat', 'long, 'og'}
+	url = 'http://datamechanics.io/data/ido_jconstan_jeansolo_suitcase/StopsLatLng.json'
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r9 = json.loads(response)
+
+	# ('lat', 'lng', 'og') of stops r9
+	t18 = project(r9, lambda t: (t['lat'], t['long']))
+
+	#parse r9 and plot the points on a map
+	latitude_list = [] 
+	longitude_list = [] 
+
+	for (x,y) in t18:
+	    latitude_list.append(x)
+	    longitude_list.append(y)
+
+	  
+	gmapBefore = gmplot.GoogleMapPlotter(42.283772, 
+	                                -71.347290, 13) 
+	  
+	# scatter points on the google map 
+	gmapBefore.scatter( latitude_list, longitude_list, '#0000FF', 
+	                              size = 40, marker = False ) 
+
+	gmapBefore.apikey = key
+
+	#gmapBefore.draw('static\\dispmap.html' ) 
+	#gmapBefore.draw('templates\\dispmap.html' )
+
+	#DATA SET 10 [k-means_0]
+	url = 'http://datamechanics.io/data/ido_jconstan_jeansolo_suitcase/k_means_school_0.json'
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r10 = json.loads(response)
+
+	#DATA SET 11 [k-means_1]
+	url = 'http://datamechanics.io/data/ido_jconstan_jeansolo_suitcase/k_means_school_1.json'
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r11 = json.loads(response)
+
+	#DATA SET 12 [k-means_2]
+	url = 'http://datamechanics.io/data/ido_jconstan_jeansolo_suitcase/k_means_school_2.json'
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r12 = json.loads(response)
+
+	#DATA SET 13 [k-means_3]
+	url = 'http://datamechanics.io/data/ido_jconstan_jeansolo_suitcase/k_means_school_3.json'
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r13 = json.loads(response)
+
+	#DATA SET 14 [k-means_4]
+	url = 'http://datamechanics.io/data/ido_jconstan_jeansolo_suitcase/k_means_school_4.json'
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r14 = json.loads(response)
+
+	#DATA SET 15 [k-means_5]
+	url = 'http://datamechanics.io/data/ido_jconstan_jeansolo_suitcase/k_means_school_5.json'
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r15 = json.loads(response)
+
+	k0 = project(r10, lambda t: (t['new_stop']))
+	k1 = project(r11, lambda t: (t['new_stop']))
+	k2 = project(r12, lambda t: (t['new_stop']))
+	k3 = project(r13, lambda t: (t['new_stop']))
+	k4 = project(r14, lambda t: (t['new_stop']))
+	k5 = project(r15, lambda t: (t['new_stop']))
+
+	temp = 0
+	for i in k0:
+		list_x = []
+		list_y = []
+
+		#for j in range(len(i)):
+		j=0
+		if i[j] == '(':
+			j+=1
+			list_x.append(i[j])
+			j+=1
+
+		while i[j] != ',':
+			list_x.append(i[j])
+			j+=1
+
+		if i[j] == ',':
+			j+=2
+
+		while i[j] != ')':
+			list_y.append(i[j])
+			j+=1
+		str_x = "".join(list_x)
+		str_y = "".join(list_y)
+		float_x = float(str_x)
+		float_y = float(str_y)
+		i = (float_x, float_y)
+
+		k0[temp] = i
+		temp += 1
+
+
+	temp = 0
+	for i in k1:
+		list_x = []
+		list_y = []
+
+		#for j in range(len(i)):
+		j=0
+		if i[j] == '(':
+			j+=1
+			list_x.append(i[j])
+			j+=1
+
+		while i[j] != ',':
+			list_x.append(i[j])
+			j+=1
+
+		if i[j] == ',':
+			j+=2
+
+		while i[j] != ')':
+			list_y.append(i[j])
+			j+=1
+		str_x = "".join(list_x)
+		str_y = "".join(list_y)
+		float_x = float(str_x)
+		float_y = float(str_y)
+		i = (float_x, float_y)
+
+		k1[temp] = i
+		temp += 1
+
+	temp = 0
+	for i in k2:
+		list_x = []
+		list_y = []
+
+		#for j in range(len(i)):
+		j=0
+		if i[j] == '(':
+			j+=1
+			list_x.append(i[j])
+			j+=1
+
+		while i[j] != ',':
+			list_x.append(i[j])
+			j+=1
+
+		if i[j] == ',':
+			j+=2
+
+		while i[j] != ')':
+			list_y.append(i[j])
+			j+=1
+		str_x = "".join(list_x)
+		str_y = "".join(list_y)
+		float_x = float(str_x)
+		float_y = float(str_y)
+		i = (float_x, float_y)
+
+		k2[temp] = i
+		temp += 1
+
+	temp = 0
+	for i in k3:
+		list_x = []
+		list_y = []
+
+		#for j in range(len(i)):
+		j=0
+		if i[j] == '(':
+			j+=1
+			list_x.append(i[j])
+			j+=1
+
+		while i[j] != ',':
+			list_x.append(i[j])
+			j+=1
+
+		if i[j] == ',':
+			j+=2
+
+		while i[j] != ')':
+			list_y.append(i[j])
+			j+=1
+		str_x = "".join(list_x)
+		str_y = "".join(list_y)
+		float_x = float(str_x)
+		float_y = float(str_y)
+		i = (float_x, float_y)
+
+		k3[temp] = i
+		temp += 1
+
+	temp = 0
+	for i in k4:
+		list_x = []
+		list_y = []
+
+		#for j in range(len(i)):
+		j=0
+		if i[j] == '(':
+			j+=1
+			list_x.append(i[j])
+			j+=1
+
+		while i[j] != ',':
+			list_x.append(i[j])
+			j+=1
+
+		if i[j] == ',':
+			j+=2
+
+		while i[j] != ')':
+			list_y.append(i[j])
+			j+=1
+		str_x = "".join(list_x)
+		str_y = "".join(list_y)
+		float_x = float(str_x)
+		float_y = float(str_y)
+		i = (float_x, float_y)
+
+		k4[temp] = i
+		temp += 1
+
+	temp = 0
+	for i in k5:
+		list_x = []
+		list_y = []
+
+		#for j in range(len(i)):
+		j=0
+		if i[j] == '(':
+			j+=1
+			list_x.append(i[j])
+			j+=1 
+
+		while i[j] != ',':
+			list_x.append(i[j])
+			j+=1
+
+		if i[j] == ',':
+			j+=2
+
+		while i[j] != ')':
+			list_y.append(i[j])
+			j+=1
+		str_x = "".join(list_x)
+		str_y = "".join(list_y)
+		float_x = float(str_x)
+		float_y = float(str_y)
+		i = (float_x, float_y)
+
+		k5[temp] = i
+		temp += 1
+
+
+	latitude_list_k0 = []
+	longitude_list_k0 = []
+	for (x,y) in k0:
+		latitude_list_k0.append(x)
+		longitude_list_k0.append(y)
+
+	latitude_list_k1 = []
+	longitude_list_k1 = []
+	for (x,y) in k1:
+		latitude_list_k1.append(x)
+		longitude_list_k1.append(y)
+
+	latitude_list_k2 = []
+	longitude_list_k2 = []
+	for (x,y) in k2:
+		latitude_list_k2.append(x)
+		longitude_list_k2.append(y)
+
+	latitude_list_k3 = []
+	longitude_list_k3 = []
+	for (x,y) in k3:
+		latitude_list_k3.append(x)
+		longitude_list_k3.append(y)
+
+	latitude_list_k4 = []
+	longitude_list_k4 = []
+	for (x,y) in k4:
+		latitude_list_k4.append(x)
+		longitude_list_k4.append(y)
+
+	latitude_list_k5 = []
+	longitude_list_k5 = []
+	for (x,y) in k5:
+		latitude_list_k5.append(x)
+		longitude_list_k5.append(y)
+
+
+	after_size = 20
+
+	# scatter points on the google map 
+	gmapBefore.scatter( latitude_list_k0, longitude_list_k0, '#FF0000', 
+	                              size = after_size, marker = False ) 
+	# scatter points on the google map 
+	gmapBefore.scatter( latitude_list_k1, longitude_list_k1, '#FF0000', 
+	                              size = after_size, marker = False ) 
+	# scatter points on the google map 
+	gmapBefore.scatter( latitude_list_k2, longitude_list_k2, '#FF0000', 
+	                              size = after_size, marker = False ) 
+	# scatter points on the google map 
+	gmapBefore.scatter( latitude_list_k3, longitude_list_k3, '#FF0000', 
+	                              size = after_size, marker = False ) 
+	# scatter points on the google map 
+	gmapBefore.scatter( latitude_list_k4, longitude_list_k4, '#FF0000', 
+	                              size = after_size, marker = False ) 
+	# scatter points on the google map 
+	gmapBefore.scatter( latitude_list_k5, longitude_list_k5, '#FF0000', 
+	                              size = after_size, marker = False ) 
+
+	gmapBefore.draw( "static\\dispmap.html" ) 
+	gmapBefore.draw( "templates\\dispmap.html" ) 
+
+	return render_template('busstops.html', name="hello")
 
 @app.after_request
 def add_header(r):
