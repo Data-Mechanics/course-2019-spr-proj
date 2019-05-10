16416dd243d96b0ea9da0b2a1ddb3e59f2933810
Andrei Lapets <a@lapets.io>
2019-05-10 17:45:57 -0400

    Move project files to group folder.

--- veeyn/getData.py
+++ veeyn/getData.py
@@ -6,12 +6,13 @@ import prov.model
 import datetime
 import uuid
 from io import StringIO
+from collections import OrderedDict
 
 class getData (dml.Algorithm):
-    contributor = 'alice_bob'
+    contributor = 'dixyTW_veeyn'
     reads = []
-    writes = ['alice_bob.bostonEthnicities']
-
+    writes = ['dixyTW_veeyn.bostonEthnicities']
+    
     @staticmethod
     def execute(trial = False):
         '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
@@ -20,25 +21,27 @@ class getData (dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
+        repo.authenticate('dixyTW_veeyn', 'dixyTW_veeyn')
         
-        #for the data set boston_Ethnicities
+        #for the data set bostonEthnicities
         url = 'http://datamechanics.io/data/dixyTW_veeyn/race_ethnicity.json'
         response = urllib.request.urlopen(url).read().decode("utf-8")
-        #r = [ json.loads(json.dumps(d)) for d in json.DictReader(StringIO(response))]
         r = json.loads(response)
         s = json.dumps(r, sort_keys=True, indent=2)
         repo.dropCollection("bostonEthnicities")
         repo.createCollection("bostonEthnicities")
-        repo['alice_bob.bostonEthnicities'].insert_one(r)
-        repo['alice_bob.bostonEthnicities'].metadata({'complete':True})
-        print(repo['alice_bob.bostonEthnicities'].metadata())
+        repo['dixyTW_veeyn.bostonEthnicities'].insert_many(r)
+        repo['dixyTW_veeyn.bostonEthnicities'].metadata({'complete':True})
+  
+        #print(repo['dixyTW_veeyn.bostonEthnicities'].metadata())
+
 
         repo.logout()
 
         endTime = datetime.datetime.now()
 
         return {"start":startTime, "end":endTime}
+        
     
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
@@ -51,39 +54,29 @@ class getData (dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
+        repo.authenticate('dixyTW_veeyn', 'dixyTW_veeyn')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:alice_bob#getData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        this_script = doc.agent('alg:dixyTW_veeyn#getData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_found, this_script)
-        doc.wasAssociatedWith(get_lost, this_script)
-        doc.usage(get_found, resource, startTime, None,
+        get_data = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_data, this_script)
+
+        doc.usage(get_data, resource, startTime, None,
                   {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
-                  }
-                  )
-        doc.usage(get_lost, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
+                  'ont:Query':''
                   }
                   )
 
-        lost = doc.entity('dat:fat60221_veeyn#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_lost, endTime)
-        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+        ethnicity = doc.entity('dat:dixyTW_veeyn#ethnicity', {prov.model.PROV_LABEL:'ethnicity', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(ethnicity, this_script)
+        doc.wasGeneratedBy(ethnicity, get_data, endTime)
+        doc.wasDerivedFrom(ethnicity, resource, get_data, get_data, get_data)
 
-        found = doc.entity('dat:fat60221_veeyn#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(found, this_script)
-        doc.wasGeneratedBy(found, get_found, endTime)
-        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
 
         repo.logout()
                   
